30ff9d4e912e7066d5700d0da7a4b844e6940bf4
gachagas <gerlito.chagas@gmail.com>
2022-08-20 23:08:21 +0800

    form align properly

--- inventory-web-app/pages/index.js
+++ inventory-web-app/pages/index.js
@@ -15,50 +15,65 @@ export default function Home() {
 
   return (
     <>
-      <main>
-        <h1>Image</h1>
-        <h1>StockFlow</h1>
+      <div className="flex justify-center h-screen ">
+        <div className=" w-auto">123</div>
 
-        <Form
-          name="basic"
-          labelCol={{ span: 8 }}
-          wrapperCol={{ span: 16 }}
-          initialValues={{ remember: true }}
-          onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
-          autoComplete="off"
+        <div
+          className="flex justify-center flex-col "
+          style={{ width: "540px" }}
         >
-          <Form.Item
-            label="Username"
-            name="username"
-            rules={[{ required: true, message: "Please input your username!" }]}
-          >
-            <Input placeholder="Username" prefix={<UserOutlined />} />
-          </Form.Item>
+          <div>456</div>
+          <div className="mx-24">
+            <Form
+              name="basic"
+              // labelCol={{ span: 8 }}
+              // wrapperCol={{ span: 16 }}
+              initialValues={{ remember: true }}
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete="off"
+            >
+              <Form.Item
+                name="username"
+                rules={[
+                  { required: true, message: "Please input your username!" },
+                ]}
+              >
+                <Input placeholder="Username" prefix={<UserOutlined />} />
+              </Form.Item>
 
-          <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: "Please input your password!" }]}
-          >
-            <Input.Password placeholder="Password" prefix={<LockOutlined />} />
-          </Form.Item>
+              <Form.Item
+                name="password"
+                rules={[
+                  { required: true, message: "Please input your password!" },
+                ]}
+              >
+                <Input.Password
+                  placeholder="Password"
+                  prefix={<LockOutlined />}
+                />
+              </Form.Item>
 
-          <Form.Item
-            name="remember"
-            valuePropName="checked"
-            wrapperCol={{ offset: 8, span: 16 }}
-          >
-            <Checkbox>Remember me</Checkbox>
-          </Form.Item>
+              <Form.Item
+                name="remember"
+                valuePropName="checked"
+                // wrapperCol={{ offset: 8, span: 16 }}
+              >
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
 
-          <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-            <Button type="primary" htmlType="submit" block>
-              Sign In
-            </Button>
-          </Form.Item>
-        </Form>
-      </main>
+              <Form.Item>
+                <Button type="primary" size="middle" htmlType="submit" block>
+                  Sign In
+                </Button>
+              </Form.Item>
+            </Form>
+          </div>
+          <div>456</div>
+        </div>
+
+        <div className=" w-auto">123</div>
+      </div>
     </>
   );
 }
